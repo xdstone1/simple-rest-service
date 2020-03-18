bf326e82857f66aef276f1c0294d047c5884fa50
steve <xdstone1@163.com>
2020-03-18 10:49:23 +0800

    version1

--- src/main/java/com/alibaba/intl/cicdk8stutorial/controllers/HelloController.java
+++ src/main/java/com/alibaba/intl/cicdk8stutorial/controllers/HelloController.java
@@ -27,7 +27,7 @@ public class HelloController {
     @RequestMapping("/")
     public String index(@RequestParam(name = "name", required = false) String name) {
         LOGGER.info("Hello service called with the name: {}", name);
-        return "Hello " + (name == null ? "world2" : name) + "!";
+        return "Hello " + (name == null ? "world1" : name) + "!";
     }
 
 }
